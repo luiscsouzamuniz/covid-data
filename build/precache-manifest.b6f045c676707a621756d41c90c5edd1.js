7d6e95b47e9ebd15a7bac714fd628afe9be64c26
Luis Carlos de Souza Muniz <luis.muniz@gett.com.br>
2020-11-20 17:10:08 -0300

    feat(covid-data): gerando build e ajustes

---
+++ build/precache-manifest.b6f045c676707a621756d41c90c5edd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fa403772d1c6d1c7baf27e73f6d3137",
+    "url": "/covid-data/index.html"
+  },
+  {
+    "revision": "0820c19877ee4a10e1ac",
+    "url": "/covid-data/static/css/2.11aa23ed.chunk.css"
+  },
+  {
+    "revision": "7df32e7fdb08693cc59b",
+    "url": "/covid-data/static/css/main.5b148a4e.chunk.css"
+  },
+  {
+    "revision": "0820c19877ee4a10e1ac",
+    "url": "/covid-data/static/js/2.c46d70c1.chunk.js"
+  },
+  {
+    "revision": "7df32e7fdb08693cc59b",
+    "url": "/covid-data/static/js/main.5eb22fdf.chunk.js"
+  },
+  {
+    "revision": "2f446708c27d1f309506",
+    "url": "/covid-data/static/js/runtime~main.26ec1e0f.js"
+  }
+]);
